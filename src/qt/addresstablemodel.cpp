a2e73c175db6cebc911bdd471dff6b62a0ee4c6a
decentralized user <decentralizeduser@gmail.com>
2018-09-01 00:23:07 +0100

    ablemodeltablemodel cpp update

--- src/qt/addresstablemodel.cpp
+++ src/qt/addresstablemodel.cpp
@@ -1,12 +1,17 @@
+// Copyright (c) 2011-2013 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include "addresstablemodel.h"
 
 #include "guiutil.h"
 #include "walletmodel.h"
 
-#include "wallet.h"
 #include "base58.h"
+#include "wallet.h"
 
 #include <QFont>
+#include <QDebug>
 
 const QString AddressTableModel::Send = "S";
 const QString AddressTableModel::Receive = "R";
