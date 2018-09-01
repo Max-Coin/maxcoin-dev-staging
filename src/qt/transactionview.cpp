d42b794c9b7549ad68fc49644599905d7184ab1a
decentralized user <decentralizeduser@gmail.com>
2018-09-01 11:58:06 +0100

    transactionview cpp update

--- src/qt/transactionview.cpp
+++ src/qt/transactionview.cpp
@@ -4,17 +4,17 @@
 
 #include "transactionview.h"
 
-#include "transactionfilterproxy.h"
-#include "transactionrecord.h"
-#include "walletmodel.h"
 #include "addresstablemodel.h"
-#include "transactiontablemodel.h"
 #include "bitcoinunits.h"
 #include "csvmodelwriter.h"
-#include "transactiondescdialog.h"
 #include "editaddressdialog.h"
-#include "optionsmodel.h"
 #include "guiutil.h"
+#include "optionsmodel.h"
+#include "transactiondescdialog.h"
+#include "transactionfilterproxy.h"
+#include "transactionrecord.h"
+#include "transactiontablemodel.h"
+#include "walletmodel.h"
 
 #include <QScrollBar>
 #include <QComboBox>
