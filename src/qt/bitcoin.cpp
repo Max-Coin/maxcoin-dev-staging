6b56f8580156d9317917de8e64dcc876b77dac41
decentralized user <decentralizeduser@gmail.com>
2018-09-09 00:01:19 +0100

    Revert "bitcoin.cpp update"
    This reverts commit c0c697fb5a5f0f35bf3d6b95ed7258b4bf63a42a.

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -2,9 +2,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#if defined(HAVE_CONFIG_H)
-#include "bitcoin-config.h"
-#endif
+#include <QApplication>
 
 #include "bitcoingui.h"
 
