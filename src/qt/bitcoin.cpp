c0c697fb5a5f0f35bf3d6b95ed7258b4bf63a42a
Oliver Morris <officialgamermorris@gmail.com>
2018-09-08 16:30:41 +0100

    bitcoin.cpp update

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -2,7 +2,9 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <QApplication>
+#if defined(HAVE_CONFIG_H)
+#include "bitcoin-config.h"
+#endif
 
 #include "bitcoingui.h"
 
