1cd8f5bbcec32beaf760386b022e25ad710542e9
oliver morris <officialgamermorris@gmail.com>
2018-10-12 01:14:05 +0100

    bitcoingui.cpp update

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -470,7 +470,8 @@ void BitcoinGUI::optionsClicked()
 {
     if(!clientModel || !clientModel->getOptionsModel())
         return;
-    OptionsDialog dlg;
+
+    OptionsDialog dlg(this);
     dlg.setModel(clientModel->getOptionsModel());
     dlg.exec();
 }
