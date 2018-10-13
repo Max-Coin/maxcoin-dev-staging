da417b989b45e3806d43adf7f44a0682b3bf7f69
oliver morris <officialgamermorris@gmail.com>
2018-10-13 23:02:00 +0100

    allocators.h update

--- src/allocators.h
+++ src/allocators.h
@@ -56,6 +56,12 @@ public:
         page_mask = ~(page_size - 1);
     }
 
+    ~LockedPageManagerBase()
+    {
+        assert(this->GetLockedPageCount() == 0);
+    }
+
+
     // For all pages in affected range, increase lock count
     void LockRange(void *p, size_t size)
     {
