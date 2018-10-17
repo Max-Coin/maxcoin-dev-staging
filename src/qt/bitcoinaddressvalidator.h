887a7ce947055f496041e587526533ad536e227b
oliver morris <officialgamermorris@gmail.com>
2018-10-17 16:11:03 +0100

    bitcoinaddressvalidator.h update

--- src/qt/bitcoinaddressvalidator.h
+++ src/qt/bitcoinaddressvalidator.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOINADDRESSVALIDATOR_H
-#define BITCOINADDRESSVALIDATOR_H
+#ifndef BITCOIN_QT_BITCOINADDRESSVALIDATOR_H
+#define BITCOIN_QT_BITCOINADDRESSVALIDATOR_H
 
 #include <QValidator>
 
