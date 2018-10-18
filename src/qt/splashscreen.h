18392703b3cef2dc3b5a6db067076b5b7e8115b3
oliver morris <officialgamermorris@gmail.com>
2018-10-19 00:35:57 +0100

    splashscreen.h update

--- src/qt/splashscreen.h
+++ src/qt/splashscreen.h
@@ -2,12 +2,18 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef SPLASHSCREEN_H
-#define SPLASHSCREEN_H
+#ifndef BITCOIN_QT_SPLASHSCREEN_H
+#define BITCOIN_QT_SPLASHSCREEN_H
 
 #include <QSplashScreen>
 
-/** class for the splashscreen with information of the running client
+class NetworkStyle;
+
+/** Class for the splashscreen with information of the running client.
+ *
+ * @note this is intentionally not a QSplashScreen. Bitcoin Core initialization
+ * can take a long time, and in that case a progress window that cannot be
+ * moved around and minimized has turned out to be frustrating to the user.
  */
 class SplashScreen : public QSplashScreen
 {
@@ -17,4 +23,4 @@ public:
     explicit SplashScreen(const QPixmap &pixmap = QPixmap(), Qt::WindowFlags f = 0);
 };
 
-#endif // SPLASHSCREEN_H
+#endif // BITCOIN_QT_SPLASHSCREEN_H
